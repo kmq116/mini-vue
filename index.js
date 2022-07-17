4c9bd618c2a40f5879e50eefe752160e1c02345b
kmq116 <521luqiya@gmail.com>
2022-07-17 22:25:52 +0800

    feat: reactive fn

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
 let activeEffect;
-const bucket = new WeakMap();
-const data = { text: "hello world", ok: false };
+const effectStack = [];
 
-const obj = new Proxy(data, {
-  get(target, key) {
-    track(target, key);
-    return target[key];
-  },
-  set(target, key, value) {
-    target[key] = value;
-    trigger(target, key);
-    return true;
-  },
-});
-effect(() => {
-  console.log("effect 读取 text 的操作", obj.text);
-  //  读取是存储副作用函数
-  document.body.innerText = obj.ok ? obj.text : "not";
-});
+const bucket = new WeakMap();
+
+export function reactive(data) {
+  return new Proxy(data, {
+    get(target, key) {
+      track(target, key);
+      return target[key];
+    },
+    set(target, key, value) {
+      target[key] = value;
+      trigger(target, key);
+      return true;
+    },
+  });
+}
 
 // 注册effect
-function effect(fn) {
+export function effect(fn) {
   const effectFn = () => {
     cleanup(effectFn);
     activeEffect = effectFn;
+    // 执行之前压入栈低 执行副作用函数的时候如果有嵌套的 effect 函数，就不会覆盖掉 activeEffect
+    effectStack.push(effectFn);
     fn();
+    // 弹栈 返回上一层的 effect 函数
+    effectStack.pop();
+    activeEffect = effectStack[effectStack.length - 1];
   };
   //利用指针将  effectFn 和 activeEffect 指向同一个 进行依赖收集
   effectFn.deps = [];
   effectFn();
 }
-setTimeout(() => {
-  obj.text = "hello proxy";
-  obj.noExist = "hello proxy";
-}, 1000);
-setTimeout(() => {
-  obj.text = "123123123";
-  obj.noExist = "hello proxy";
-  console.log(bucket);
-}, 2000);
 
 function cleanup(effectFn) {
   for (let i = 0; i < effectFn.deps.length; i++) {
@@ -64,6 +58,13 @@ function trigger(target, key) {
   const depsMap = bucket.get(target);
   if (!depsMap) return;
   const effects = depsMap.get(key);
-  const effectsToRun = new Set(effects);
+  // 这里选择一个新的内存来执行依赖函数 防止触发 effect 函数后无限循环
+  const effectsToRun = new Set();
+  effects &&
+    effects.forEach((effectFn) => {
+      if (effectFn !== activeEffect) {
+        effectsToRun.add(effectFn);
+      }
+    });
   effectsToRun && effectsToRun.forEach((fn) => fn());
 }
